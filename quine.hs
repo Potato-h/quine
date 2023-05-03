abdd63780b0286d3c94c06d72b3fd10be341bb57
Ilya Shpilkov <ilya.shpilkov@mail.ru>
2023-05-03 21:35:59 +0300

    Fix newline

--- quine.hs
+++ quine.hs
@@ -16,4 +16,4 @@ main = do
     let raw = "{-# LANGUAGE OverloadedStrings #-}\n\nimport Data.Text as T\n\nescape :: Text -> Text\nescape = T.concatMap escapeChar\n    where\n        escapeChar \'\\n\' = \"\\\\n\"\n        escapeChar \'\\\'\' = \"\\\\\\\'\"\n        escapeChar \'\"\'  = \"\\\\\\\"\"\n        escapeChar \'\\\\\' = \"\\\\\\\\\"\n        escapeChar c    = T.singleton c\n\nmain :: IO ()\nmain = do\n    let raw = \"?\"\n    let (pref, qsuff) = breakOn \"?\" raw\n    let suff = T.drop 1 qsuff\n    putStrLn . unpack $ T.concat [pref, escape raw, suff]\n"
     let (pref, qsuff) = breakOn "?" raw
     let suff = T.drop 1 qsuff
-    putStrLn . unpack $ T.concat [pref, escape raw, suff]
+    putStr . unpack $ T.concat [pref, escape raw, suff]
